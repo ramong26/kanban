c99b835c4ec15b1bc15119cd8027093939561b4c
ramong26 <rlatndus21049@gmail.com>
2026-01-15 18:59:18 +0900

    chore: App - 필요없는 코드 삭제

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -14,15 +14,3 @@ function App() {
 }
 
 export default App;
-
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <h1 className="text-3xl font-bold underline text-amber-400">
-//       Hello world!
-//     </h1>
-//   );
-// }
-
-// export default App;
