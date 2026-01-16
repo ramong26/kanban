4bbf6e42845c6b397a32574e58cecd5c4cac38e9
ramong26 <rlatndus21049@gmail.com>
2026-01-16 17:20:53 +0900

    feat: Header - 검색 로직 디바운스 적용

--- src/shared/components/Layout/Header/Header.tsx
+++ src/shared/components/Layout/Header/Header.tsx
@@ -1,11 +1,22 @@
+import { useCallback, useEffect, useState } from "react";
 import { useSearchStore } from "../../../../stores/searchStore";
 
 export default function Header() {
+  // Search Handler
   const { query, setQuery } = useSearchStore();
+  const [searchQuery, setSearchQuery] = useState(query);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-  };
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  }, []);
+
+  useEffect(() => {
+    const debouncedSave = setTimeout(() => {
+      setQuery(searchQuery);
+    }, 300);
+
+    return () => clearTimeout(debouncedSave);
+  }, [searchQuery, setQuery]);
 
   return (
     <header className="bg-gradient-to-r from-purple-200 via-pink-200 to-blue-200 p-8 shadow-sm">
@@ -17,7 +28,7 @@ export default function Header() {
           type="search"
           placeholder="검색..."
           className="px-6 py-3 rounded-2xl border-2 border-purple-200 bg-white/80 backdrop-blur-sm focus:outline-none focus:ring-2 focus:ring-purple-300 focus:border-purple-300 text-gray-700 placeholder-gray-400 transition-all w-96"
-          value={query}
+          value={searchQuery}
           onChange={handleChange}
         />
       </div>
