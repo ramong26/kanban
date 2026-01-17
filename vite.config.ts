8c2bd42796d99bed8b521d908177b79374c9e979
ramong26 <rlatndus21049@gmail.com>
2026-01-17 21:56:33 +0900

    fix: 빌드에러 수정 - Vite(런타임/빌드) alias를 인식하도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/kanban/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
